0f58b9f1e79ea9e0da0b29c438f649946325697b
Vishal Jadhav <112711686+Vc555s@users.noreply.github.com>
2025-04-26 15:34:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database', 'coinnect.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///coinnect.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Filebase configuration
+    FILEBASE_ACCESS_KEY = os.environ.get('FILEBASE_ACCESS_KEY', '')
+    FILEBASE_SECRET_KEY = os.environ.get('FILEBASE_SECRET_KEY', '')
+    
+    # IPFS Gateway URL - for viewing files
+    IPFS_GATEWAY_URL = 'https://ipfs.filebase.io/ipfs/'
